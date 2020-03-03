034adfc2d7de9b289b4b0271efc89261ee5c3e6a
Jonathan Yong <yongrenjie@gmail.com>
2020-03-03 12:05:21 +0000

    Linting
    Problem : Code doesn't conform to PEP8.
    Solution: Make it conform with PEP8 (thanks to PyCQA/pycodestyle).
              Closes #5.

--- pypopt-makecf.py
+++ pypopt-makecf.py
@@ -2,7 +2,7 @@
 try:
     ERRMSG("pypopt-makecf.py: Please run this script outside TopSpin!")
     EXIT()
-except:
+except NameError:
     pass
 # TODO: Can we get TopSpin to run this from system Python 3 anyway?
 
@@ -20,7 +20,8 @@ def main():
     Main routine for creation of a new cost function.
     """
     # Instructions:
-    # 1. Define cf_name (this will be the name you choose when creating a new routine in TopSpin)
+    # 1. Define cf_name (this will be the name you choose for the cost function
+    #    when creating a new routine in TopSpin).
     # 2. Edit f() to return the value you want it to.
     # 3. Run this script in Python 3.
 
@@ -31,23 +32,29 @@ def main():
     def f():
         # f() must return the value of the cost function as a float.
         # Some builtin methods are available:
-        #  - getpar(param) takes a string and returns the value of the parameter as a float
-        #  - get_real_spectrum() returns the real spectrum as a numpy array of size SI.
-        #      You can pass two arguments, left and right, to specify a region of the spectrum.
-        #      For example:
+        #  - getpar(param)       takes a string and returns the value of the
+        #                        parameter as a float.
+        #  - get_real_spectrum() returns the real spectrum as a numpy array
+        #                        of size SI. You can pass two arguments, "left"
+        #                        and "right", to specify a particular region
+        #                        of the spectrum of interest.
+        #           For example:
         #           - get_real_spectrum() OR get_real_spectrum(None, None)
         #             return the entire spectrum.
         #           - get_real_spectrum(6.21, None)
-        #             returns the part of the spectrum with chemical shift <= 6.21
+        #             returns the part of the spectrum with shift <= 6.21
         #             (i.e. the spectrum is left-bounded by 6.21 ppm).
         #           - get_real_spectrum(None, 6.21)
-        #             returns the part of the spectrum with chemical shift >= 6.21.
+        #             returns the part of the spectrum with shift >= 6.21.
         #           - get_real_spectrum(6.21, 4)
-        #             returns the part of the spectrum between 6.21 ppm and 4 ppm.
+        #             returns the part of the spectrum between 6.21 and 4 ppm.
         #  - get_imag_spectrum() does the same for the imaginary spectrum.
-        #  - get_fid() returns the complex-valued FID as a numpy array of size TD/2.
+        #  - get_fid()           returns the complex-valued FID as a numpy
+        #                        array of size TD/2.
 
-        # In this short example, we simply sum the intensities at each point of the real spectrum. This gives us a quick and easy measure of the total signal intensity.
+        # In this short example, we simply sum the intensities at each point
+        # of the real spectrum. This gives us a quick and easy measure of the
+        # total signal intensity.
         return np.sum(get_real_spectrum())
 
     # STEP 3
