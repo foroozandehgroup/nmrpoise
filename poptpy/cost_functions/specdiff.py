25a6b874e949269d6e61dc80f2d35a5f72c85555
Jonathan Yong <yongrenjie@gmail.com>
2020-05-15 14:44:31 +0800

    Cost function for distance between 2 spectra
    e.g. for PSYCHE optimisation

---
+++ poptpy/cost_functions/specdiff.py
@@ -0,0 +1,4 @@
+def cost_function():
+    target = get_real_spectrum(epno=[1,1])
+    spec = get_real_spectrum()
+    return np.linalg.norm(target/np.linalg.norm(target) - spec/np.linalg.norm(spec))
