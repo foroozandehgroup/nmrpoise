f3d78c050ecba9691aa03b1d6b4d6559c528a7a9
Jonathan Yong <yongrenjie@gmail.com>
2020-05-31 21:33:12 +0800

    Implement multidirectional search optimiser (#19)
    Problem:  Only Nelder-Mead was available.
    Solution: Implement another optimiser, namely the multidirectional
              search method of Torczon (1989). However, it's difficult to
              write unit tests for it, because it converges ultra slowly...
              (Fixes #19)

--- poptpy/poptpy_backend/poptimise.py
+++ poptpy/poptpy_backend/poptimise.py
@@ -56,18 +56,20 @@ class Simplex():
             # Rosenbrock with x0 = [1.3, 0.7, 0.8, 1.9, 1.2]: 705 nfev, 431 nit
             # (average over 1000 iterations)
             self.x[0] = self.x0
+            rng = np.random.default_rng()
             for i in range(1, self.N + 1):
-                self.x[i] = np.random.rand(self.N)
+                self.x[i] = rng.uniform(size=self.N)
         elif method == "jon":
             # My own method. It isn't very good. Like, it works, but it's slow.
             # Rosenbrock with x0  = [1.3, 0.7, 0.8, 1.9, 1.2]: 669 nfev, 409 nit
             # (average over 1000 iterations)
             self.x[0] = self.x0
+            rng = np.random.default_rng()
             for k in range(self.N):
                 # Each point x_i (\neq x_0) is equal to x_0 \pm [0.2, 0.4).
                 y = self.x0.copy()
-                y = self.x0.copy() + ((np.random.rand(self.N)*0.2 + 0.2) *
-                                      np.sign(np.random.randn(self.N)))
+                y = y + (rng.uniform(0.2, 0.4, self.N) *
+                         rng.choice([1, -1], self.N))
                 self.x[k + 1] = y
         else:
             raise ValueError("invalid simplex generation method "
@@ -112,6 +114,14 @@ class Simplex():
             self.x[i] = self.x[0] - (self.x[i] - self.x[0])/2
 
 
+# Custom exceptions.
+class MaxFevalsReached(Exception):
+    pass
+
+class MaxItersReached(Exception):
+    pass
+
+
 # Function counter decorator. We should probably keep this here and not in
 # backend, so that we can reuse code (because of the input() calls to set
 # global variables, backend cannot be imported).
@@ -140,7 +150,8 @@ class OptResult:
 
 def nelder_mead(cf, x0, xtol, args=(), plot=False, simplex_method="spendley"):
     """
-    Nelder-Mead optimiser.
+    Nelder-Mead optimiser, as described in Section 8.1 of Kelley, "Iterative
+    Methods for Optimization".
 
     Inputs to this function should already be scaled to have lower and upper
     bounds of 0 and 1.
@@ -174,6 +185,7 @@ def nelder_mead(cf, x0, xtol, args=(), plot=False, simplex_method="spendley"):
         o.simplex (ndarray) : (N+1, N)-sized matrix of the final simplex.
         o.fvals (ndarray)   : List of corresponding cost functions at each point
                               of the simplex.
+        o.message (str)     : Message indicating reason for termination.
     """
 
     # Convert x0 to vector
@@ -243,91 +255,265 @@ def nelder_mead(cf, x0, xtol, args=(), plot=False, simplex_method="spendley"):
         plot_iters, plot_fvals = [], []
 
     # Main loop.
-    while not converged(sim, xtol):
-        niter += 1
-        sim.sort()  # for good measure
+    try:
+        while not converged(sim, xtol):
+            niter += 1
+            sim.sort()  # for good measure
 
-        # Plotting
-        if plot:
-            plot_iters.append(niter)
-            plot_fvals.append(sim.f[0])
-            plt.cla()
-            plt.xlabel("Iteration number")
-            plt.ylabel("Cost function")
-            plt.plot(plot_iters, plot_fvals)
-            plt.pause(0.0001)
+            # Plotting
+            if plot:
+                plot_iters.append(niter)
+                plot_fvals.append(sim.f[0])
+                plt.cla()
+                plt.xlabel("Iteration number")
+                plt.ylabel("Cost function")
+                plt.plot(plot_iters, plot_fvals)
+                plt.pause(0.0001)
 
-        # Check number of iterations
-        if niter >= maxiter:
-            break
+            # Check number of iterations
+            if niter >= maxiter:
+                raise MaxItersReached
 
-        # Step 3(a)
-        x_r = xnew(mu_r, sim)  # shorthand for x(mu_r)
-        f_r = cf(x_r, *args)
+            # Step 3(a)
+            x_r = xnew(mu_r, sim)  # shorthand for x(mu_r)
+            f_r = cf(x_r, *args)
 
-        # Step 3(b): Reflect (+ 3g if needed)
-        if cf.calls >= maxfev:
-            break
-        if sim.f[0] <= f_r and f_r < sim.f[N - 1]:
-            sim.replace_worst(x_r, f_r)
-            sim.sort()  # Step 3(g)
-            continue
-
-        # Step 3(c): Expand (+ 3g if needed)
-        if cf.calls >= maxfev:
-            break
-        if f_r < sim.f[0]:
-            x_e = xnew(mu_e, sim)
-            f_e = cf(x_e, *args)
-            if f_e < f_r:
-                sim.replace_worst(x_e, f_e)
-            else:
+            # Step 3(b): Reflect (+ 3g if needed)
+            if cf.calls >= maxfev:
+                raise MaxFevalsReached
+            if sim.f[0] <= f_r and f_r < sim.f[N - 1]:
                 sim.replace_worst(x_r, f_r)
-            sim.sort()  # Step 3(g)
-            continue
-
-        # Step 3(d): Outside contraction (+ 3f and 3g if needed)
-        if cf.calls >= maxfev:
-            break
-        if sim.f[N - 1] <= f_r and f_r < sim.f[N]:
-            x_oc = xnew(mu_oc, sim)
-            f_c = cf(x_oc, *args)
-            if f_c <= f_r:
-                sim.replace_worst(x_oc, f_c)
-                sim.sort()  # Step 3(g)
-                continue
-            else:
-                if cf.calls >= maxfev - N:             # Step 3(f)
-                    break
-                sim.shrink()
-                for i in range(1, N + 1):
-                    sim.f[i] = cf(sim.x[i], *args)
                 sim.sort()  # Step 3(g)
                 continue
 
-        # Step 3(e): Inside contraction (+ 3f and 3g if needed)
-        if cf.calls >= maxfev:
-            break
-        if f_r >= sim.f[N]:
-            x_ic = xnew(mu_ic, sim)
-            f_c = cf(x_ic, *args)
-            if f_c < sim.f[N]:
-                sim.replace_worst(x_ic, f_c)
+            # Step 3(c): Expand (+ 3g if needed)
+            if cf.calls >= maxfev:
+                raise MaxFevalsReached
+            if f_r < sim.f[0]:
+                x_e = xnew(mu_e, sim)
+                f_e = cf(x_e, *args)
+                if f_e < f_r:
+                    sim.replace_worst(x_e, f_e)
+                else:
+                    sim.replace_worst(x_r, f_r)
                 sim.sort()  # Step 3(g)
                 continue
-            else:
-                if cf.calls >= maxfev - N:             # Step 3(f)
-                    break
-                sim.shrink()
-                for i in range(1, N + 1):
-                    sim.f[i] = cf(sim.x[i], *args)
-                sim.sort()  # Step 3(g)
-                continue
-    # END while loop
+
+            # Step 3(d): Outside contraction (+ 3f and 3g if needed)
+            if cf.calls >= maxfev:
+                raise MaxFevalsReached
+            if sim.f[N - 1] <= f_r and f_r < sim.f[N]:
+                x_oc = xnew(mu_oc, sim)
+                f_c = cf(x_oc, *args)
+                if f_c <= f_r:
+                    sim.replace_worst(x_oc, f_c)
+                    sim.sort()  # Step 3(g)
+                    continue
+                else:
+                    if cf.calls >= maxfev - N:             # Step 3(f)
+                        raise MaxFevalsReached
+                    sim.shrink()
+                    for i in range(1, N + 1):
+                        sim.f[i] = cf(sim.x[i], *args)
+                    sim.sort()  # Step 3(g)
+                    continue
+
+            # Step 3(e): Inside contraction (+ 3f and 3g if needed)
+            if cf.calls >= maxfev:
+                raise MaxFevalsReached
+            if f_r >= sim.f[N]:
+                x_ic = xnew(mu_ic, sim)
+                f_c = cf(x_ic, *args)
+                if f_c < sim.f[N]:
+                    sim.replace_worst(x_ic, f_c)
+                    sim.sort()  # Step 3(g)
+                    continue
+                else:
+                    if cf.calls >= maxfev - N:             # Step 3(f)
+                        raise MaxFevalsReached
+                    sim.shrink()
+                    for i in range(1, N + 1):
+                        sim.f[i] = cf(sim.x[i], *args)
+                    sim.sort()  # Step 3(g)
+                    continue
+        # END while loop
+    except MaxItersReached:
+        message = "Maximum iterations reached."
+    except MaxFevalsReached:
+        message = "Maximum function evaluations reached."
+    else:
+        message = "Optimisation terminated successfully."
 
     # sort the simplex in ascending order of fvals
     sim.sort()
 
     return OptResult(xbest=sim.x[0], fbest=sim.f[0],
                      niter=niter, nfev=cf.calls,
-                     simplex=sim.x, fvals=sim.f)
+                     simplex=sim.x, fvals=sim.f,
+                     message=message)
+
+
+def multid_search(cf, x0, xtol, args=(), plot=False, simplex_method="spendley"):
+    """
+    Multidimensional search optimiser, as described in Secion 8.2 of Kelley,
+    "Iterative Methods for Optimization".
+
+    Inputs to this function should already be scaled to have lower and upper
+    bounds of 0 and 1.
+
+    Arguments:
+        cf:   The cost function.
+        args: A tuple of arguments to pass to the cost function.
+        x0:   List containing initial point for optimisation.
+        xtol: List containing tolerances for each parameter.
+        plot: Bool. If true then this plots the cost function value against
+              function evaluation. NOTE: This makes the optimisation run a lot
+              more slowly (~ 3 orders of magnitude!!). If this is coupled to
+              NMR acquisition, though, this isn't an issue, since we go from
+              ~ ms to ~ s.
+        simplex_method: Method for generation of initial simplex.
+
+    Returns:
+        OptResult object 'o' which contains the following attributes:
+
+        o.xbest (ndarray)   : Optimal values for the optimisation.
+        o.fbest (float)     : Cost function at the optimum.
+        o.niter (int)       : Number of iterations.
+        o.nfev (int)        : Number of function evaluations. Note that in the
+                              specific context of NMR optimisation, this is in
+                              general not equal to the number of experiments
+                              acquired.
+        o.simplex (ndarray) : (N+1, N)-sized matrix of the final simplex.
+        o.fvals (ndarray)   : List of corresponding cost functions at each point
+                              of the simplex.
+        o.message (str)     : Message indicating reason for termination.
+    """
+    # Convert x0 to vector
+    x0 = np.asfarray(x0).flatten()
+    xtol = np.asfarray(xtol).flatten()
+    N = x0.size
+
+    # Default maxiter and maxfev. We could make this customisable in future.
+    # For example, we could use TopSpin's `expt' to calculate the duration of
+    # one experiment, and then set maxiter to not overshoot a given time.
+    maxiter = 200 * N
+    maxfev = 500 * N
+
+    # Check length of xtol
+    if len(x0) != len(xtol):
+        raise ValueError("Multidimensional search: x0 and xtol have "
+                         "incompatible lengths")
+    # Calculate geometric mean of xtol and make sure that it's sensible
+    xtol_gm = np.prod(xtol) ** (1 / np.size(xtol))
+    if xtol_gm > 0.35:
+        raise ValueError("Multidimensional search: xtol is too large. "
+                         "Please reduce the tolerances.")
+
+    # Create and initialise simplex object.
+    sim = Simplex(x0, method=simplex_method, length=max(0.3, xtol_gm))
+    # Number of iterations. Function evaluations are stored as cf.calls.
+    niter = 0
+
+    # Set up parameters for Nelder-Mead.
+    mu_e = 2       # Expansion parameter
+    mu_c = 0.5     # Contraction parameter
+
+    def converged(sim, xtol):
+        """
+        Convergence criteria. To be converged, each dimension of the simplex
+        must have a range smaller than or equal to the corresponding xtol in
+        that dimension.
+        """
+        simplex_range = np.amax(sim.x, axis=0) - np.amin(sim.x, axis=0)
+        return all(np.less_equal(simplex_range, xtol))
+        ## Scipy convergence criteria. Assumes that the simplex is already
+        ## sorted. It is slightly looser (i.e. will converge before mine),
+        ## but hardly makes a difference to the average fevals (tested on
+        ## Rosenbrock function).
+        # return np.max(np.ravel(np.abs(sim[1:] - sim[0]))) <= xtol[0]
+
+    # Decorate cost function so that it keeps tracks of nfev.
+    cf = deco_count(cf)
+
+    # Evaluate the cost function for the initial simplex.
+    # Steps 1 and 2 in Algorithm 8.2.1
+    for i in range(N + 1):
+        sim.f[i] = cf(sim.x[i], *args)
+    # Sort simplex
+    sim.sort()
+
+    # Plotting initialisation.
+    if plot:
+        import matplotlib.pyplot as plt
+        plot_iters, plot_fvals = [], []
+
+    # Main loop.
+    try:
+        while not converged(sim, xtol):
+            niter += 1
+            sim.sort()  # for good measure
+
+            # Plotting
+            if plot:
+                plot_iters.append(niter)
+                plot_fvals.append(sim.f[0])
+                plt.cla()
+                plt.xlabel("Iteration number")
+                plt.ylabel("Cost function")
+                plt.plot(plot_iters[-100:], plot_fvals[-100:])
+                plt.pause(0.0001)
+
+            # Check number of iterations and function evaluations
+            if niter >= maxiter:
+                raise MaxItersReached
+            if cf.calls >= maxfev:
+                raise MaxFevalsReached
+
+            # Step 3(a): Reflect
+            r_j = np.zeros((N, N))
+            f_r_j = np.zeros(N)
+            for j in range(1, N + 1):
+                r_j[j - 1] = sim.x[0] - (sim.x[j] - sim.x[0])
+                f_r_j[j - 1] = cf(r_j[j - 1], *args)
+
+            # Step 3(b): Expand
+            if sim.f[0] > np.amin(f_r_j):
+                e_j = np.zeros((N, N))
+                f_e_j = np.zeros(N)
+                for j in range(1, N + 1):
+                    e_j[j - 1] = sim.x[0] - mu_e * (sim.x[j] - sim.x[0])
+                    f_e_j[j - 1] = cf(e_j[j - 1], *args)
+                # Replace the values, 3(b)(ii)
+                if np.amin(f_r_j) > np.amin(f_e_j):
+                    for j in range(1, N + 1):
+                        sim.x[j] = e_j[j - 1]
+                        sim.f[j] = f_e_j[j - 1]
+                else:
+                    for j in range(1, N + 1):
+                        sim.x[j] = r_j[j - 1]
+                        sim.f[j] = f_r_j[j - 1]
+                sim.sort()  # Step 3(d)
+                continue
+            # Step 3(c): Contract
+            else:
+                for j in range(1, N + 1):
+                    sim.x[j] = sim.x[0] + mu_c * (sim.x[j] - sim.x[0])
+                    sim.f[j] = cf(sim.x[j], *args)
+                sim.sort()  # Step 3(d)
+                continue
+    except MaxItersReached:
+        message = "Maximum iterations reached."
+    except MaxFevalsReached:
+        message = "Maximum function evaluations reached."
+    else:
+        message = "Optimisation terminated successfully."
+
+    # sort the simplex in ascending order of fvals
+    sim.sort()
+
+    return OptResult(xbest=sim.x[0], fbest=sim.f[0],
+                     niter=niter, nfev=cf.calls,
+                     simplex=sim.x, fvals=sim.f,
+                     message=message)
+
+
--- tests/test_opt.py
+++ tests/test_opt.py
@@ -1,21 +1,64 @@
 import numpy as np
-from scipy.optimize import rosen
+import pytest
 
-from poptpy.poptpy_backend.poptimise import nelder_mead
+from poptpy.poptpy_backend.poptimise import nelder_mead, multid_search
+
+def rosenbrock(x):
+    # Using scipy's definition.
+    return sum(100.0*(x[1:] - x[:-1]**2.0)**2.0 + (1 - x[:-1])**2.0)
 
 
 x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
 xtol = np.array([1e-6] * len(x0))
-optResult = nelder_mead(cf=rosen, x0=x0, xtol=xtol,
-                        simplex_method="spendley")
-
+simplex_method="spendley"
+nexpt = 1 if simplex_method in ["spendley", "axis"] else 50
 
 def testNMAccuracy():
+    optResult = nelder_mead(cf=rosenbrock, x0=x0, xtol=xtol,
+                            simplex_method=simplex_method)
     assert np.all(np.isclose(optResult.xbest, np.ones(len(x0)), atol=1e-6))
 
 def testNMIters():
-    # very lax, but depending on the simplex method this is necessary
-    assert optResult.niter < 450
+    count = 0
+    for i in range(nexpt):
+        optResult = nelder_mead(cf=rosenbrock, x0=x0, xtol=xtol,
+                                simplex_method=simplex_method)
+        count += optResult.niter
+    count /= nexpt
+    assert count < 450
 
 def testNMFevals():
-    assert optResult.nfev < 750
+    count = 0
+    for i in range(nexpt):
+        optResult = nelder_mead(cf=rosenbrock, x0=x0, xtol=xtol,
+                                simplex_method=simplex_method)
+        count += optResult.nfev
+    count /= nexpt
+    assert count < 750
+
+
+# MDS is very difficult to test with Rosenbrock because it converges awfully
+# slowly. See: Torczon (1989). https://scholarship.rice.edu/handle/1911/16304
+
+# def testMDSAccuracy():
+#     optResult = multid_search(cf=rosenbrock, x0=x0, xtol=xtol,
+#                               simplex_method=simplex_method)
+#     assert np.all(np.isclose(optResult.xbest, np.ones(len(x0)), atol=1e-6))
+
+# def testMDSIters():
+#     count = 0
+#     for i in range(nexpt):
+#         optResult = multid_search(cf=rosenbrock, x0=x0, xtol=xtol,
+#                                   simplex_method=simplex_method)
+#         count += optResult.niter
+#     count /= nexpt
+#     assert count < 450
+
+# def testMDSFevals():
+#     count = 0
+#     for i in range(nexpt):
+#         optResult = multid_search(cf=rosenbrock, x0=x0, xtol=xtol,
+#                                   simplex_method=simplex_method)
+#         count += optResult.nfev
+#     count /= nexpt
+#     assert count < 750
