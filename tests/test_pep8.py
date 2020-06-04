9a405309c47f7504c0c1f708b48f62724fda21b8
Jonathan Yong <yongrenjie@gmail.com>
2020-06-04 16:34:24 +0800

    Tests: make style errors no longer cumulative

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -2,22 +2,23 @@ from pathlib import Path
 
 import pycodestyle
 
-s = pycodestyle.StyleGuide()
-
 
 def test_style_frontend():
+    s = pycodestyle.StyleGuide()
     p = (Path(__file__).parents[1].resolve()) / "poptpy"
     res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
     assert res.total_errors == 0
 
 
 def test_style_backend():
+    s = pycodestyle.StyleGuide()
     p = (Path(__file__).parents[1].resolve()) / "poptpy" / "poptpy_backend"
     res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
     assert res.total_errors == 0
 
 
 def test_style_costfunctions():
+    s = pycodestyle.StyleGuide()
     p = ((Path(__file__).parents[1].resolve()) / "poptpy" / "poptpy_backend" /
          "cost_functions")
     res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
@@ -25,6 +26,7 @@ def test_style_costfunctions():
 
 
 def test_style_tests():   # how meta
+    s = pycodestyle.StyleGuide()
     p = Path(__file__).parent
     res = s.check_files([f for f in p.iterdir() if f.suffix == ".py"])
     assert res.total_errors == 0
