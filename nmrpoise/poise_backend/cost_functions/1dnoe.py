383e6372d3bc96cf0282238c18c0064ff02ef90d
Jonathan Yong <yongrenjie@gmail.com>
2020-08-19 16:15:57 +0800

    Fix 1D NOE cost function
    It was still using the old function prototype for get1d_real().

--- nmrpoise/poise_backend/cost_functions/1dnoe.py
+++ nmrpoise/poise_backend/cost_functions/1dnoe.py
@@ -2,7 +2,7 @@ def cost_function():
     f = getpar("SPOFFS2") + getpar("O1")   # frequency of selective pulse
     bw = 50.0                              # rough bandwidth of selective pulse
     sfo1 = getpar("SFO1")
-    maxshift = (f + bw/2.0)/sfo1
-    minshift = (f - bw/2.0)/sfo1
-    return -abs(np.sum(get1d_real(None, maxshift)) +
-                np.sum(get1d_real(minshift, None)))
+    upperhalf = f"{(f + bw/2)/sfo1:.3f}.."
+    lowerhalf = f"..{(f - bw/2)/sfo1:.3f}"
+    return -abs(np.sum(get1d_real(bounds=upperhalf)) +
+                np.sum(get1d_real(bounds=lowerhalf)))
