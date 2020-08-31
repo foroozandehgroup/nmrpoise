35209b22b68387244bf448aceca67869e45c0029
Jonathan Yong <yongrenjie@gmail.com>
2020-09-01 00:51:22 +0800

    Add documentation to _g attributes

--- nmrpoise/poise_backend/shared.py
+++ nmrpoise/poise_backend/shared.py
@@ -14,6 +14,40 @@ from pathlib import Path
 class _g():
     """
     Class to store the "global" variables.
+
+    Attributes
+    ----------
+    optimiser : str from {'nm', 'mds', 'bobyqa'}
+        The optimiser being used.
+
+    routine_id : str
+        The name of the routine being used.
+
+    p_spectrum : pathlib.Path
+        The path to the procno folder of the spectrum being acquired. (e.g.
+        ``/path/to/data/1/pdata/1``)
+
+    p_optlog : pathlib.Path
+        The path to the currently active ``poise.log`` file.
+
+    p_errlog : pathlib.Path
+        The path to the currently active ``poise_err_backend.log`` file.
+
+    maxfev : int
+        The maximum number of function evaluations specified by the user. Can
+        be zero, indicating no limit (beyond the hard limit of 500 times the
+        number of parameters).
+
+    p_poise : pathlib.Path
+        The path to the ``$TS/exp/stan/nmr/py/user/poise_backend`` folder.
+
+    spec_f1p : float or tuple of float
+        The ``F1P`` parameter. For a 1D spectrum this is a float. For a 2D
+        spectrum this is a tuple of floats (``indirect, direct``) corresponding
+        to the values of ``F1P`` in both spectral dimensions.
+
+    spec_f2p : float or tuple of float
+        The ``F2P`` parameter.
     """
     optimiser = None
     routine_id = None
